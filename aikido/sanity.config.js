429949ea3e670b6f2cca7bce7553fdf3b9813957
rohan <rohan.kamboj@hcode.tech>
2023-01-31 18:39:21 +0530

    sanity setup

---
+++ aikido/sanity.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './schemas';
+
+export default defineConfig({
+  name: 'Aikido_Studio',
+  title: 'Aikido Studio',
+
+  projectId: 'bcsd31u5',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
