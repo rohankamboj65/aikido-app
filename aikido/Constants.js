1458c374733a80cb5209ed837637f75402fa4a09
mainkahcode <mainka.dhankher@hcode.tech>
2023-02-01 15:38:49 +0530

    investing section

---
+++ aikido/Constants.js
@@ -0,0 +1,27 @@
+export const faq_pricing_page = [
+    {
+      tagName:'h2',
+      question: 'What kind of brand efforts do you specialise in?',
+      answer:
+        'PayTunes has been a pioneer in Audio advertising. PayTunes by Real Time Network Private Limited is an audio advertising platform that aggregates the audio advertisement inventory of all the Indian music app. It is the number 1 ad aggregator in this category and hence are the biggest Audio Advertisement revenue contributors in India to publishers.',
+    },
+    {
+      tagName:'h2',
+      question: 'How do you measure the results of your marketing efforts?',
+      answer:
+        'Our operations team keeps a track of and shares a daily campaign report. Every Impression can be tracked. Even though we have a robust tracking system of our own, we are open to apply any third-party trackers like DCM, Sizmek etc. which allows a high level of transparency and trust between PayTunes and Client.',
+    },
+    {
+      tagName: 'h3',
+      question:
+        ' Do you have a portfolio or samples of your past campaigns or client work?',
+      answer:
+        'You can understand our client range and past campaigns by visiting our Youtube page to view the past client campaigns.',
+    },
+    {
+      tagName: 'h5',
+      question: ' What is your pricing model or how do you charge for services?',
+      answer:
+        'Every campaign varies in its deliverables, to know more details get in touch with us at info@paytunes.in or reach us at +91-8448330304 ',
+    },
+  ]
--- aikido/pages/index.js
+++ aikido/pages/index.js
@@ -1,11 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import Navbar from '@/Components/Navbar'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Navbar from "@/Components/Navbar";
+import Accordion from "@/Components/Accordian";
+import { faq_pricing_page } from "@/Constants";
+import curveBlackImage from '../Images/curveBlackImage.png'
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -16,8 +18,58 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar/>
-    
+      <Navbar />
+      <div className="investBackground">
+
+      <Accordion data={faq_pricing_page} />
+
+      {/* Get essential investing tips */}
+      <div
+      className=""
+        style={{
+          width: "100%",
+          textAlign: 'center',
+          // alignItems: "center",
+          // justifyContent: "center",
+          margin: "20px 0",
+          // gap: "10px",
+          color: 'white',
+          
+        
+
+        }}
+      >
+          
+        <div >
+          <p style={{ fontWeight: 600, fontSize: "50px", margin: 0 }}>
+            GET ESSENTIAL <br /> INVESTING TIPS
+          </p>
+          <p style={{ fontWeight: 400, fontSize: "14px" ,margin:0 }}>
+            SUBSCRIBE TO OUR NEWSLETTER FOR FREE TODAY
+          </p>
+        </div>
+        <div className=""
+          // style={{
+          //   // display: 'flex',
+          //   gap: "10px",
+          //   backgroundImage: `url(${curveBlackImage.src})`,
+          //   backgroundRepeat: "no-repeat",
+          //   backgroundSize: "cover",
+          //   width: "100%",
+          //   height: "100vh",
+          // }}
+        >
+          <input
+            type="email"
+            placeholder="Enter your e-mail address"
+            style={{ height: "30px" }}
+          />
+          <button className="primary-button">SIGN UP</button>
+        </div>
+      </div>
+      </div>
+
+     
     </>
-  )
+  );
 }
